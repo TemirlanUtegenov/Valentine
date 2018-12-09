bf57c44e25a567ed8cd3a8d224bd4bf3c3f56f93
Stud01 <Stud01@localhost>
2018-12-09 10:24:10 +0300

    slide checking added category info added

--- app/test/test_valentine.py
+++ app/test/test_valentine.py
@@ -1,3 +1,4 @@
+import json
 import time
 
 import pytest
@@ -5,8 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
-
-base_url = 'http://awful-valentine.com/'
 
 
 @pytest.fixture(scope='module')  # фикстура
@@ -21,9 +20,17 @@ def driver():
     driver.quit()
 
 
-def test_slider_image_click_shows_product_page(driver: WebDriver):
+@pytest.fixture(scope='module')  # создали фикстуру чтобы с помощью ее брать данные из env в папке env
+def environment():
+    env = json.load(open('./env/env.json'))
+    return env['test']
+
+
+def test_slider_first_image_click_shows_product_page(driver: WebDriver, environment):
+    base_url = environment['base_url']
     driver.get(base_url)
 
+    # Ничего не оптимизируйте, пока в этом нет необходимости
     featured_title_el = driver.find_element(By.CSS_SELECTOR, '.featured-title')  # с точки потому что имя класса CSS
     title = featured_title_el.text
 
@@ -35,5 +42,65 @@ def test_slider_image_click_shows_product_page(driver: WebDriver):
     category_title_el = driver.find_element(By.CSS_SELECTOR, '.category-title')
     assert title == category_title_el.text
 
-    driver.save_screenshot('./screenshot.png') #создается скриншот. Обновляется после каждого теста
+    driver.save_screenshot('./screenshot.png')  # создается скриншот. Обновляется после каждого теста
 
+
+def test_slider_second_image_click_shows_product_page(driver: WebDriver, environment):
+    goto_homepage(driver, environment)
+    select_slide(driver, 2)
+    slide_info = get_slide_info(driver, 2)
+    click_slide_image(driver, 2)
+
+    # NoSuchElementException: элемент на странице не найден
+    # ElementNotVisibleException: элемент не видим, взаимодействовать (кликать) с ним нельзя
+    # ReferenceStalenessException: сначала нашли, а потом он исчез
+
+    assert slide_info['title'] in driver.title
+    category_info = get_category_info(driver)
+    assert slide_info['title'] == category_info['title']
+
+
+def goto_homepage(driver, environment):
+    base_url = environment['base_url']
+    driver.get(base_url)
+
+
+def select_slide(driver, slide_no):
+    slide_handle_el = driver.find_elements(
+        By.CSS_SELECTOR, '#controllers > a'
+    )[slide_no - 1]
+
+    slide_handle_el.click()
+
+
+def get_slide_info(driver, slide_no):
+    info = {}  # создаём пустой dict
+    slide_el = driver.find_elements(
+        By.CSS_SELECTOR, '.slide'
+    )[slide_no - 1]
+
+    featured_title_el = slide_el.find_element(
+        By.CSS_SELECTOR, '.featured-title'
+    )
+    info['title'] = featured_title_el.text
+
+    price_el = slide_el.find_element(
+        By.CSS_SELECTOR, '.price'
+    )
+    info['price'] = price_el.text
+
+    return info
+
+
+def click_slide_image(driver, slide_no):
+    featured_image_el = driver.find_elements(
+        By.CSS_SELECTOR, '.featured-image'
+    )[slide_no - 1]
+    featured_image_el.click()
+
+
+def get_category_info(driver):
+    info = {}
+    category_title_el = driver.find_element(By.CSS_SELECTOR, '.category-title')
+    info['title'] = category_title_el.text
+    return info
